78e76107f2c61b54d9b85289b6508fddbc4124d1
Abubakker Siddiq <abubakkersiddiqn@gmail.com>
2022-06-28 13:54:43 +0530

    process.env

--- index.js
+++ index.js
@@ -2,14 +2,16 @@ import express from 'express';
 import { MongoClient } from "mongodb";
 import { AmazonData } from './Routes/AmazonRoutes.js'
 import cors from 'cors'
+import dotenv from 'dotenv'
 
+ dotenv.config();
 const app = express();
-const PORT =5001
+
 app.use(express.json())
 app.use(cors())
-const MONGO_URL= "mongodb+srv://abu123:abu123456@cluster0.vmj1f.mongodb.net"
+
 async function createConnection(){
-                         const client = new MongoClient(MONGO_URL);
+                         const client = new MongoClient(process.env.MONGO_URL);
                          await client.connect();
                         console.log("sucessfully connected");
                         return client;  
@@ -23,5 +25,5 @@ async function createConnection(){
 
 app.use('/',AmazonData)
 
-app.listen(PORT,()=>console.log('listening on port no.5001'))
+app.listen(process.env.PORT,()=>console.log('listening on port'))
 
